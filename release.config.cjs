47ce81baff838fe18d571431ea2c0a1a6083dda5
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2025-04-18 01:04:41 +0100

    chore: actions/upload-pages-artifact@v3 update

--- release.config.cjs
+++ release.config.cjs
@@ -1,24 +1,4 @@
 /* eslint-disable */
 module.exports = {
-  plugins: [
-    {
-      "@semantic-release/commit-analyzer": {
-        preset: "conventionalcommits",
-      },
-    },
-    {
-      "@semantic-release/release-notes-generator": {
-        preset: "conventionalcommits",
-      },
-    },
-    "@semantic-release/npm",
-    "@semantic-release/changelog",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-      },
-    ],
-    "@semantic-release/github",
-  ],
+  preset: 'conventionalcommits',
 };
